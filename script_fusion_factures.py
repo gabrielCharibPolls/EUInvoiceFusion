24872293f6b892d907ed27ebc8f6d06820688d3a
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2024-03-04 11:30:44 +0100

    🔍 Ajout vérif. extension fichier pour MAJ ID 🆔 + barre recherche 🕵️‍♂️
    - Vérifie la terminaison du fichier   avant MAJ ID dans fusion 🔄
    - Intègre barre de recherche pour suivi efficace 🔎

--- script_fusion_factures.py
+++ script_fusion_factures.py
@@ -2,6 +2,8 @@ import os
 import glob
 import pandas as pd
 import json
+from collections import defaultdict
+import shutil  # Importez shutil pour déplacer des fichiers
 
 
 ########################################################
@@ -28,12 +30,30 @@ def load_json(file_name):
 def save_json(file_name, data):
     with open(file_name, 'w') as f:
         json.dump(data, f)
-
+#################################################
+# Extrait le numéro à la fin du nom de fichier 
+#################################################
+        
+def extract_file_number(filename):
+    # Extrait le numéro à la fin du nom de fichier (avant l'extension)
+    base_name = os.path.basename(filename)
+    try:
+        return int(base_name.split('_')[-1].split('.')[0])
+    except ValueError:
+        return 0
 # Chemins et noms de fichiers
 folder_path = os.getcwd()
 pattern = "Invoice Header Report by Supplier Group EU_*.xlsx"
 state_file = 'files_state.json'
 fused_files_file = 'fused_files.json'
+
+
+backup_folder = os.path.join(folder_path, 'backup')  # Dossier de sauvegarde
+############################################################
+# verfier si  sauvegarde existe et créé si c'est pas le cas 
+############################################################
+if not os.path.exists(backup_folder):
+    os.makedirs(backup_folder)
 
 #############################################
 #état précédent et les fichiers déjà fusionnés
@@ -61,7 +81,36 @@ if missing_files:
     for file in missing_files:
         print(file)
 
+################################################################################################################
+##tenir au compte que de la dernier trascation no par rapport a la terminesion du fichier + grand plus recent 
+###############################################################################################################
+transaction_file_mapping = defaultdict(list)
+for file in new_files:
+    df = pd.read_excel(file, sheet_name=0)
+    for index, row in df.iterrows():
+        transaction_no = row['TRANSACTION NO']
+        transaction_file_mapping[transaction_no].append((file, extract_file_number(file), index))
+
+
+#####################################################################
+# Sélectionner l'entrée avec le numéro de fichier le plus élevé
+#####################################################################
+selected_entries = []
+for transaction_no, files in transaction_file_mapping.items():
+    
+    selected_file = max(files, key=lambda x: x[1])
+    selected_entries.append((selected_file[0], selected_file[2]))
+#####################################################################
+# Fusionner les données sélectionnées
+#####################################################################
+data_frames = []
+for file, index in selected_entries:
+    df = pd.read_excel(file, sheet_name=0)
+    data_frames.append(df.loc[[index]])
+
+#####################################################################
 # Fusionner uniquement les nouveaux fichiers
+#####################################################################
 data_frames = []
 for file in new_files:
     df = pd.read_excel(file, sheet_name=0)
@@ -79,10 +128,11 @@ else:
 # Sauvegarder les fichiers fusionnés
 save_json(fused_files_file, fused_files)
 
-# Supprimer les fichiers fusionnés
-for file in new_files:
+for file, index in selected_entries:
+    base_file_name = os.path.basename(file)
+    destination = os.path.join(backup_folder, base_file_name)
     try:
-        os.remove(file)
-        print(f"Le fichier {file} a été supprimé.")
-    except OSError as e:
-        print(f"Erreur lors de la suppression du fichier {file}: {e.strerror}")
+        shutil.move(file, destination)
+        print(f"Le fichier {file} a été déplacé vers {destination}.")
+    except Exception as e:
+        print(f"Erreur lors du déplacement du fichier {file}: {e}")
