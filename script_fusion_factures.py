6d90af323d0b4e337e1d436b2d8a32b11ce6da16
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2024-03-05 16:21:03 +0100

    rajoute de comentaires plus detailles

--- script_fusion_factures.py
+++ script_fusion_factures.py
@@ -3,21 +3,17 @@ import glob
 import pandas as pd
 import json
 from collections import defaultdict
-import shutil  # Importez shutil pour déplacer des fichiers
+import shutil  #déplacer des fichiers
 
 
-########################################################
+#######################################################################################
 #fusionne automatiquement des fichiers Excel de factures 
 #basés sur un motif de nom de fichier spécifique, 
 #puis met à jour et sauvegarde l'état de ces 
 #fichiers pour éviter les traitements répétitifs.
-######################################################
-
-
-
-###########################################
-#charger le fichers json
-###########################################
+#######################################################################################
+#charger le fichers json                                                              #
+#######################################################################################
 def load_json(file_name):
     try:
         with open(file_name, 'r') as f:
@@ -30,9 +26,27 @@ def load_json(file_name):
 def save_json(file_name, data):
     with open(file_name, 'w') as f:
         json.dump(data, f)
-#################################################
-# Extrait le numéro à la fin du nom de fichier 
-#################################################
+########################################################################################
+# Extraction du Numéro à la Fin du Nom de Fichier (Avant l'Extension)                  #
+########################################################################################
+#
+# Objectif :
+# ----------
+# Identifier et extraire le numéro situé à la fin du nom d'un fichier, juste avant 
+# l'extension. Ce numéro est crucial car, dans le contexte de fichiers fusionnés, 
+# il représente souvent un identifiant unique (ID) qui doit être le dernier ID mentionné.
+#
+# Importance :
+# ------------
+# L'extraction de cet ID permet de gérer et d'identifier correctement les fichiers,
+# en assumant que le numéro le plus récent (et donc le dernier dans l'ordre) est 
+# représentatif de la version ou de l'instance la plus à jour du fichier concerné.
+#
+# Exemple :
+# ---------
+# Nom de fichier : "rapport_financier_14015585.xlsx"
+# ID extrait : 14015585
+########################################################################################
         
 def extract_file_number(filename):
     # Extrait le numéro à la fin du nom de fichier (avant l'extension)
@@ -47,51 +61,77 @@ pattern = "Invoice Header Report by Supplier Group EU_*.xlsx"
 state_file = 'files_state.json'
 fused_files_file = 'fused_files.json'
 
-
-backup_folder = os.path.join(folder_path, 'backup')  # Dossier de sauvegarde
-############################################################
-# verfier si  sauvegarde existe et créé si c'est pas le cas 
-############################################################
+######################################################################################## 
+# Dossier de sauvegarde
+backup_folder = os.path.join(folder_path, 'backup') 
+########################################################################################
+# verfier si  sauvegarde existe ,créé si c'est pas le cas 
+########################################################################################
 if not os.path.exists(backup_folder):
     os.makedirs(backup_folder)
 
-#############################################
+########################################################################################
 #état précédent et les fichiers déjà fusionnés
-#############################################
+########################################################################################
 previous_state = load_json(state_file)
 fused_files = load_json(fused_files_file)
 
-#############################################
+
+########################################################################################
 # Trouver les fichiers actuels
-#############################################
+########################################################################################
 files_found = [file for file in glob.glob(os.path.join(folder_path, pattern))]
 
+########################################################################################
 # Mettre à jour l'état actuel et sauvegarder
+########################################################################################
 current_state = {os.path.basename(file): True for file in files_found}
 save_json(state_file, current_state)
 
+########################################################################################
 # Identifier les fichiers nouveaux ou manquants
 new_files = [file for file in files_found if os.path.basename(file) not in fused_files]
 missing_files = [file for file in previous_state if file not in current_state]
-#############################################
+
+########################################################################################
 # Signaler les fichiers manquants
-#############################################
+########################################################################################
 if missing_files:
     print("Fichier(s) manquant(s) depuis la dernière exécution :")
     for file in missing_files:
         print(file)
-
-################################################################################################################
-##tenir au compte que de la dernier trascation no par rapport a la terminesion du fichier + grand plus recent 
-###############################################################################################################
+####################################################################################
+# Gestion des Données dans les Fichiers Excel liés aux Transactions Commerciales   #
+####################################################################################
+#
+# Structure des données :
+# -----------------------
+# - Colonne "Invoice_Number" : Numéro de facture pour chaque transaction.
+# - Colonne "Buyer_TN" (Transaction Number de l'acheteur) : Identifiant de l'acheteur.
+#
+# Note Importante :
+# -----------------
+# Un même "Invoice_Number" peut être lié à un unique "Buyer_TN", mais correspondre à 
+# plusieurs "Transaction_Number". Cela indique la possibilité de réaliser plusieurs 
+# transactions sous une seule facture pour un acheteur donné.
+#
+# Exemple de Structure de Données :
+#  ------------------------------------------------
+# | Invoice_Number | Buyer_TN | Transaction_Number |
+# |----------------|----------|--------------------|
+# |     12345      |   TN001  |        TR001       |
+# |     12345      |   TN001  |        TR002       |
+# |     12346      |   TN002  |        TR003       |
+#  ------------------------------------------------
+#
+# Ce tableau montre que la facture 12345, liée à l'acheteur TN001, couvre deux transactions
+# distinctes (TR001 et TR002). Cela illustre la gestion flexible des transactions commerciales.
 transaction_file_mapping = defaultdict(list)
 for file in new_files:
     df = pd.read_excel(file, sheet_name=0)
     for index, row in df.iterrows():
         transaction_no = row['TRANSACTION NO']
         transaction_file_mapping[transaction_no].append((file, extract_file_number(file), index))
-
-
 #####################################################################
 # Sélectionner l'entrée avec le numéro de fichier le plus élevé
 #####################################################################
